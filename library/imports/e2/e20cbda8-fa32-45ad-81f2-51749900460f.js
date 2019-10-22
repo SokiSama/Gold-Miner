f649179f010a76baa91859499010453d3bd0de55
Sokia <sokisama0@gmail.com>
2019-10-22 09:37:40 +0800

    Add files via upload

---
+++ library/imports/e2/e20cbda8-fa32-45ad-81f2-51749900460f.js
@@ -0,0 +1,48 @@
+"use strict";
+cc._RF.push(module, 'e20cb2o+jJFrYHyUXSZAEYP', 'go game');
+// JavaScript/go game.js
+
+"use strict";
+
+// Learn cc.Class:
+//  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/class.html
+//  - [English] http://docs.cocos2d-x.org/creator/manual/en/scripting/class.html
+// Learn Attribute:
+//  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/reference/attributes.html
+//  - [English] http://docs.cocos2d-x.org/creator/manual/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/life-cycle-callbacks.html
+//  - [English] https://www.cocos2d-x.org/docs/creator/manual/en/scripting/life-cycle-callbacks.html
+
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        Lighting: {
+            type: cc.Node,
+            default: null
+        },
+        BGM: {
+            default: null,
+            url: cc.AudioClip
+        }
+    },
+
+    // LIFE-CYCLE CALLBACKS:
+
+    // onLoad () {},
+
+    StartGame: function StartGame() {
+        //关闭BGM
+        cc.audioEngine.stop(this.BGM_ID);
+        //清空关卡数
+        cc.sys.localStorage.removeItem('level');
+        //跳转场景
+        cc.director.loadScene("Game");
+    }
+}
+
+// update (dt) {},
+);
+
+cc._RF.pop();
